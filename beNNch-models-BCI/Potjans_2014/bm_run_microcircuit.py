bea2994aaa35487599294f8200cc370034ec664e
ValeriaGamez <vgamez@usc.edu>
2025-12-13 00:45:01 -0800

    data gen

---
+++ beNNch-models-BCI/Potjans_2014/bm_run_microcircuit.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+#
+# bm_run_microcircuit.py
+
+"""PyNEST Microcircuit: Run Benchmark Simulation
+--------------------------------------------------
+
+This is an example script for running the microcircuit model.
+This version is adjusted for benchmark simulations. Since spikes are
+usually not recorded in this scenario, the evaluation part with plotting of
+'run_microcircuit.py' is not performed here.
+
+"""
+
+###############################################################################
+# Import the necessary modules and start the time measurements.
+
+from stimulus_params import stim_dict
+from network_params import net_dict
+from sim_params import sim_dict
+from bm_helpers import logging, memory
+import network
+import nest
+import time
+import numpy as np
+import stimulus
+
+time_start = time.time()
+
+###############################################################################
+# Initialize the network with simulation, network and stimulation parameters,
+# then create and connect all nodes, and finally simulate.
+# The times for a presimulation and the main simulation are taken
+# independently. A presimulation is useful because the spike activity typically
+# exhibits a startup transient. In benchmark simulations, this transient should
+# be excluded from a time measurement of the state propagation phase. Besides,
+# statistical measures of the spike activity should only be computed after the
+# transient has passed.
+#
+# Benchmark: In contrast to run_microcircuit.py, some default simulation and
+# network parameters are here overwritten.
+
+
+sim_dict.update({
+    't_presim': {model_time_presim}, #presim phase
+    't_sim': {model_time_sim}, #only the simulation phase
+    'rec_dev': [{record_spikes}],
+    'rng_seed': {rng_seed},
+    'local_num_threads': {threads_per_task},
+    'print_time': False,
+    'kwds': {kwds}})
+
+net_dict.update({
+    'N_scaling': {scale_N},
+    'K_scaling': {scale_K},
+    'poisson_input': {poisson_input},
+    'V0_type': {V0_type},
+    'synapse_type': {synapse_type}})
+
+stimulus.update_sim_dict()
+
+py_timers = {}
+memory_used = {}
+
+memory_used['base_memory'] = memory()
+
+t0 = time.time()
+net = network.Network(sim_dict, net_dict, stim_dict)
+t1 = time.time()
+py_timers['py_time_network'] = t1 - t0
+
+net.create()
+t2 = time.time()
+py_timers['py_time_create'] = t2 - t1
+memory_used['node_memory'] = memory()
+
+net.connect()
+t3 = time.time()
+py_timers['py_time_connect'] = t3 - t2
+memory_used['network_memory'] = memory()
+
+net.simulate(sim_dict['t_presim'])
+t4 = time.time()
+py_timers['py_time_presimulate'] = t4 - t3
+memory_used['init_memory'] = memory()
+
+intermediate_kernel_status = nest.GetKernelStatus()
+
+net.simulate(sim_dict['t_sim'])
+t5 = time.time()
+py_timers['py_time_simulate'] = t5 - t4
+memory_used['total_memory'] = memory()
+
+###############################################################################
+# Plot a spike raster of the simulated neurons and a box plot of the firing
+# rates for each population.
+# For visual purposes only, spikes 100 ms before and 100 ms after the thalamic
+# stimulus time are plotted here by default.
+# The computation of spike rates discards the presimulation time to exclude
+# initialization artifacts.
+
+raster_plot_interval = np.array([stimulus.getStartms(),
+                                 stimulus.getStartms() + 200])
+firing_rates_interval = np.array([stimulus.getStartms(),
+                                  stimulus.getTotalms()])
+net.evaluate(raster_plot_interval, firing_rates_interval)
+time_evaluate = time.time()
+
+
+
+###############################################################################
+# Summarize time measurements. Rank 0 usually takes longest because of print
+# calls.
+
+print(
+    '\nTimes of Rank {}:\n'.format(
+        nest.Rank()) +
+    '  Total time:          {:.3f} s\n'.format(
+        py_timers['py_time_simulate']) +
+    '  Time to initialize:  {:.3f} s\n'.format(
+        py_timers['py_time_network']) +
+    '  Time to create:      {:.3f} s\n'.format(
+        py_timers['py_time_create']) +
+    '  Time to connect:     {:.3f} s\n'.format(
+        py_timers['py_time_connect']) +
+    '  Time to presimulate: {:.3f} s\n'.format(
+        py_timers['py_time_presimulate']) +
+    '  Time to simulate:    {:.3f} s\n'.format(
+        py_timers['py_time_simulate']))
+
+###############################################################################
+# Query the accumulated number of spikes on each rank.
+
+local_spike_counter = net.get_local_spike_counter()
+num_neurons = net.get_network_size()
+rate = 1. * local_spike_counter / num_neurons / net.get_total_sim_time() * 1000
+mem = memory()
+
+print(
+    'local_spike_counter: {}'.format(
+        local_spike_counter))
+
+print(
+    'Number of neurons: {}'.format(
+        num_neurons))
+
+print(
+    'Rate per rank: {}'.format(
+        rate))
+
+print(
+    'memory: {}'.format(
+        mem))
+
+logging(py_timers=py_timers, memory_used=memory_used, intermediate_kernel_status=intermediate_kernel_status)
---
+++ beNNch-models-BCI/multi-area-model/run_benchmark_createParams.py
@@ -0,0 +1,88 @@
+import numpy as np
+import os
+import sys
+import json
+import nest
+
+from multiarea_model import MultiAreaModel, MultiAreaModel_3
+from config import base_path
+from start_jobs import start_job
+from figures.Schmidt2018_dyn.network_simulations import NEW_SIM_PARAMS
+
+N_scaling = float(sys.argv[1])
+num_processes = int(sys.argv[2])
+num_local_threads = int(sys.argv[3])
+t_sim = float(sys.argv[4])
+K_scaling = float(sys.argv[5])
+data_path = sys.argv[6]
+data_folder_hash = sys.argv[7]
+# Fig3: corresponds to figure 3 in schmidt et al. 2018: Groundstate
+mam_state = sys.argv[8]
+# Fig5: corresponds to figure 5 in schmidt et al. 2018: Metastable
+rng_seed = int(sys.argv[9])
+t_presim = float(sys.argv[10])
+record_spikes = sys.argv[11] == 'True'
+
+if mam_state == 'ground':
+    figure = 'Fig3'
+elif mam_state == 'metastable':
+    figure = 'Fig5'
+else:
+    raise KeyError('No network state selected. Choose between "ground" and\
+                   "metastable" state in the config file.')
+network_params, _ = NEW_SIM_PARAMS[figure][0]
+
+network_params['connection_params']['K_stable'] = os.path.join(
+    base_path, 'K_stable.npy')
+network_params['N_scaling'] = N_scaling
+network_params['K_scaling'] = K_scaling
+network_params['fullscale_rates'] = os.path.join(
+    base_path, 'tests/fullscale_rates.json')
+
+sim_params = {'t_sim': t_sim,
+              't_presim': t_presim,
+              'num_processes': num_processes,
+              'local_num_threads': num_local_threads,
+              'recording_dict': {'record_vm': False}}
+
+if not record_spikes:
+    sim_params['recording_dict']['areas_recorded'] = []
+
+theory_params = {'dt': 0.1}
+
+os.mkdir(os.path.join(data_path, data_folder_hash))
+
+try:
+    nest.version()
+    NEST_version = '2'
+except:
+    nest.__version__
+    NEST_version = '3'
+
+if NEST_version == '2':
+    print("NEST version 2.x\n")
+    sim_params['master_seed'] = rng_seed
+    M = MultiAreaModel(network_params, simulation=True,
+                       sim_spec=sim_params,
+                       theory=True,
+                       theory_spec=theory_params,
+                       data_path=data_path,
+                       data_folder_hash=data_folder_hash)
+elif NEST_version == '3':
+    print("NEST version 3.0\n")
+    sim_params['rng_seed'] = rng_seed
+    M = MultiAreaModel_3(network_params, simulation=True,
+                         sim_spec=sim_params,
+                         theory=True,
+                         theory_spec=theory_params,
+                         data_path=data_path,
+                         data_folder_hash=data_folder_hash)
+
+print(M.label)
+print(M.simulation.label)
+
+p, r = M.theory.integrate_siegert()
+print("Mean-field theory predicts an average "
+      "rate of {0:.3f} spikes/s across all populations.".format(np.mean(r[:, -1])))
+
+start_job(M.simulation.label, data_path, data_folder_hash)
